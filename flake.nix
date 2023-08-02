51fddee7bb0bd456613e5afc6e618a421b1d00d8
Bradley Nelson <bradely@nel.family>
2023-08-01 20:45:12 -0600

    streamdeck a basice pathfinder

--- flake.nix
+++ flake.nix
@@ -13,6 +13,8 @@
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
     # TODO: Add any other flake you might need
     # hardware.url = "github:nixos/nixos-hardware";
 
