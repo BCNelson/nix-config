9ca2777ff1c5af4aef8ac2fc125cfd55f019d6ca
Bradley Nelson <bradely@nel.family>
2023-09-06 01:00:15 -0600

    add simple disko

---
+++ disko/default.nix
@@ -0,0 +1,32 @@
+{disk, ...}: {
+  disko.devices = {
+    disk = {
+      vdb = {
+        device = ${disk};
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "100M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
