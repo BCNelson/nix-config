3e8c70341e978c652f80a0b1aa44cc9e79623479
Bradley Nelson <bradely@nel.family>
2023-09-29 01:03:21 -0600

    start tmux start fish

--- nixos/_mixins/roles/server/default.nix
+++ nixos/_mixins/roles/server/default.nix
@@ -4,7 +4,17 @@
         ../docker.nix
     ];
 
-    environment.systemPackages = [
-        pkgs.tmux
-    ];
+    programs.tmux = {
+        enable = true;
+        extraConfig = ''
+            # Enable mouse
+            set -g mode-mouse on
+            setw -g mouse-resize-pane on
+            setw -g mouse-select-window on
+            setw -g mouse-select-pane on
+
+            set -g default-command /usr/local/bin/fish
+            set -g default-shell /usr/local/bin/fish
+        '';
+    };
 }
