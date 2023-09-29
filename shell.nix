a52e3a463c551cdcdb0ae10195207e0803eccbfa
Bradley Nelson <bradely@nel.family>
2023-09-28 18:42:15 -0600

    remove github cil from dev env

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [ nix home-manager git git-crypt just gh ] 
+    nativeBuildInputs = with pkgs; [ nix home-manager git git-crypt just ] 
     ++ lib.optional (lib.hasInfix system == "linux") pkgs.quickemu;
   };
 }
