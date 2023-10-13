9632503afc3ebb4fd5455c062e1e15ca5f41da74
Bradley Nelson <bradley@nel.family>
2023-10-13 17:01:19 -0600

    add nextcloud to romeo

--- nixos/romeo/services.nix
+++ nixos/romeo/services.nix
@@ -91,6 +91,32 @@ in
                 ];
                 restart = "unless-stopped";
             };
+            nextcloud = {
+                image = "ghcr.io/linuxserver/nextcloud:latest";
+                container_name = "nextcloud";
+                environment = [
+                    "PUID=1000"
+                    "PGID=1000"
+                    "TZ=America/Denver"
+                ];
+                volumes = [
+                    "${dataDirs.level5}/nextcloud/config:/config"
+                    "${dataDirs.level5}/nextcloud/data:/data"
+                ];
+                links = [ "nextcloud_db:db" ];
+                restart = "unless-stopped";
+            };
+            nextcloud_db = {
+                image = "postgres:12";
+                restart = "unless-stopped";
+                container_name = "nextcloud_db";
+                volumes = [
+                    "${dataDirs.level5}/nextcloud/database:/var/lib/postgresql/data"
+                ];
+                environment = [
+                    "POSTGRES_PASSWORD=${sensitiveData.nextcloud_postgress_password}"
+                ];
+            };
         };
     };
   };
