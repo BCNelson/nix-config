be35b90a0d62be96e51912dd18bcae81c88e1c4f
Bradley Nelson <bradely@nel.family>
2023-10-15 11:04:50 -0600

    remove unnessary comment

--- nixos/_mixins/users/bcnelson/default.nix
+++ nixos/_mixins/users/bcnelson/default.nix
@@ -8,7 +8,6 @@ in
     isNormalUser = true;
     description = "Bradley Nelson";
     extraGroups = [ "networkmanager" "wheel" "plugdev" "docker" "dialout" ];
-    # Hard coding this password is probably a bad idea but I don't want to decrypt the vault on install just for this.
     # The install script will change mark this user as needing a password change on first login.
     # Note SDDM does not support password changes so this will need to be done via the command line.
     initialPassword = sensitive.bcnelson_init_password;
