b94d924653e6626325ab71a4591157090e422cf0
Bradley Nelson <bradely@nel.family>
2023-10-16 14:48:41 -0600

    add gaming and tailscale to xray

--- nixos/xray/default.nix
+++ nixos/xray/default.nix
@@ -3,5 +3,8 @@
   imports = [
     ./hardware-configuration.nix
     ../_mixins/roles/docker.nix
+    ../_mixins/roles/gaming.nix
+    ../_mixins/roles/tailscale.nix
+    ../_mixins/roles/desktop
   ];
 }
