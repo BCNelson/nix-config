e78896235d8ad221c254627c7f40f0cfbb97488a
Bradley Nelson <bradleynelson102@gmail.com>
2023-11-22 14:34:49 -0800

    add syncthing to romeo

--- nixos/romeo/services.nix
+++ nixos/romeo/services.nix
@@ -153,6 +153,20 @@ in
           ];
           restart = "unless-stopped";
         };
+        syncthing = {
+          image = "ghcr.io/linuxserver/syncthing";
+          container_name = "syncthing";
+          environment = [
+            "PUID=1000"
+            "PGID=1000"
+            "TZ=America/Denver"
+          ];
+          volumes = [
+            "${dataDirs.level5}/syncthing/config:/config"
+            "${dataDirs.level4}/syncthing/data:/data/folders/level4"
+          ];
+          restart = "unless-stopped";
+        };
       };
     };
   };
