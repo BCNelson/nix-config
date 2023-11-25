e532781d82f42cbac6d5c52c542c2661e75f23c5
Bradley Nelson <bradleynelson102@gmail.com>
2023-11-25 14:01:44 -0800

    add samba to vor

---
+++ nixos/vor/samba.nix
@@ -0,0 +1,39 @@
+_:
+{
+    services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+    networking.firewall.allowedTCPPorts = [
+        5357 # wsdd
+    ];
+    networking.firewall.allowedUDPPorts = [
+        3702 # wsdd
+    ];
+    openFirewall = true;
+    services.samba = {
+        enable = true;
+        securityType = "user";
+        extraConfig = ''
+            workgroup = WORKGROUP
+            server string = vor
+            netbios name = vor
+            security = user 
+            #use sendfile = yes
+            #max protocol = smb2
+            hosts allow = 100.64.0.0/10 192.168.138.0/24
+            hosts deny = 0.0.0.0/0
+            guest account = nobody
+            map to guest = bad user
+        '';
+        shares = {
+            public = {
+                path = "/liveData/NelsonData/Nelson Family Files";
+                browseable = "yes";
+                "read only" = "no";
+                "guest ok" = "yes";
+                "create mask" = "0644";
+                "directory mask" = "0755";
+                "force user" = "bcnelson";
+                "force group" = "bcnelson";
+            };
+        };
+    };
+}
