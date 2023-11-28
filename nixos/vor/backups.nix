733bb2ab762e9889db7bde54c952b69d1a778cbf
Bradley Nelson <bradely@nel.family>
2023-11-28 13:46:20 -0700

    vor local replication

--- nixos/vor/backups.nix
+++ nixos/vor/backups.nix
@@ -6,10 +6,19 @@ _:
             "liveData/NelsonData" = {
                 hourly = 72;
                 daily = 31;
+                weekly = 26;
+                monthly = 12;
+                yearly = 5;
+                useTemplate = [ "common" ];
+            };
+            "vault/Backups/Nelson Family Data" = {
+                hourly = 72;
+                daily = 31;
                 weekly = 52;
                 monthly = 24;
                 yearly = 10;
                 useTemplate = [ "common" ];
+                autosnap = false;
             };
         };
         templates = {
@@ -17,6 +26,15 @@ _:
                 autoprune = true;
                 autosnap = true;
                 recursive = true;
+            };
+        };
+    };
+    services.syncoid = {
+        enable = true;
+        commands = {
+            "liveData/NelsonData Local Backup" = {
+                source = "liveData/NelsonData";
+                target = "vault/Backups/Nelson Family Data";
             };
         };
     };
