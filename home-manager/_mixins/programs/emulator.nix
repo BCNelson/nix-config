35021280be10ac0eddb16b50702fe9ed95e12238
Bradley Nelson <bradely@nel.family>
2023-12-02 10:18:09 -0700

    add some emulators to sierra

---
+++ home-manager/_mixins/programs/emulator.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}:
+{
+    home.packages = with pkgs; [
+        (retroarch.override {
+            cores = with libretro; [
+                pcsx2
+                parallel-n64
+                dolphin
+                desmume
+            ];
+        })
+    ];
+}
--- home-manager/bcnelson/sierra.nix
+++ home-manager/bcnelson/sierra.nix
@@ -5,5 +5,6 @@
     ../_mixins/programs/deckmaster
     ../_mixins/programs/jetbrains/rider.nix
     ../_mixins/programs/jetbrains/goland.nix
+    ../_mixins/programs/emulator.nix
   ];
 }
