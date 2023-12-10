9b2ed56949928afa77a5d02c9b2951887a564d85
Bradley Nelson <bradely@nel.family>
2023-12-10 00:31:18 -0700

    add udev rules for gamecube controller to usb in wii mode

--- nixos/_mixins/roles/emulator.nix
+++ nixos/_mixins/roles/emulator.nix
@@ -3,9 +3,9 @@ _:
 {
   services.udev = {
     enable = true;
-    # Enable Stream Deck support
     extraRules = ''
       SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="0079", ATTRS{idProduct}=="1843", MODE="0666"
+      SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
     '';
   };
 }
