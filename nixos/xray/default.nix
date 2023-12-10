28e6dc96f02036e8e1bd84e61b9098ef6acc5ccd
Bradley Nelson <bradely@nel.family>
2023-12-10 16:20:31 -0700

    kdeconnect ports for zray

--- nixos/xray/default.nix
+++ nixos/xray/default.nix
@@ -7,4 +7,15 @@
     ../_mixins/roles/tailscale.nix
     ../_mixins/roles/desktop
   ];
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [
+      { from = 9090; to = 9100; } # local services
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; } # KDE Connect
+    ];
+    allowedTCPPorts = [ 22000 ]; # Syncthing
+    allowedUDPPorts = [ 22000 21027 ]; # Syncthing
+  };
 }
