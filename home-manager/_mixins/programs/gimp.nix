da161e758db65d37bce067f2a8884eeff8ff231a
Bradley Nelson <bradely@nel.family>
2023-12-17 11:46:59 -0700

    formatting fixes

--- home-manager/_mixins/programs/gimp.nix
+++ home-manager/_mixins/programs/gimp.nix
@@ -1,6 +1,6 @@
-{pkgs,...}:
+{ pkgs, ... }:
 {
-    home.packages = with pkgs; [
-        gimp
-    ];
+  home.packages = with pkgs; [
+    gimp
+  ];
 }
--- nixos/sierra/hardware-configuration.nix
+++ nixos/sierra/hardware-configuration.nix
@@ -65,7 +65,7 @@
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  
+
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
