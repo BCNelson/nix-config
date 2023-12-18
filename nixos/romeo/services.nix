07d1730197a8b70cb1b1d2cd9f463af066aadf67
Bradley Nelson <bradely@nel.family>
2023-12-17 17:02:45 -0700

    add homebox to romeo

--- nixos/romeo/services.nix
+++ nixos/romeo/services.nix
@@ -210,7 +210,20 @@ in
             "${dataDirs.level7}/fastenhealth/cache:/opt/fasten/cache"
           ];
         };
-
+        homeBox = {
+          image = "ghcr.io/hay-kot/homebox:latest";
+          container_name = "homebox";
+          restart = "unless-stopped";
+          environment = [
+            "HBOX_LOG_LEVEL=info"
+            "HBOX_LOG_FORMAT=text"
+            "HBOX_WEB_MAX_UPLOAD_SIZE=50"
+            "HBOX_OPTIONS_ALLOW_REGISTRATION=false"
+          ];
+          volumes = [
+            "${dataDirs.level3}/homeBox:/data/"
+          ];
+        };
       };
     };
   };
