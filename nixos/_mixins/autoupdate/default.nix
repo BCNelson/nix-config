3ddc78f432376c262220205091b98e89bb28e406
Bradley Nelson <bradely@nel.family>
2024-01-06 22:24:04 -0700

    add nixos-rebuild to auto-update

--- nixos/_mixins/autoupdate/default.nix
+++ nixos/_mixins/autoupdate/default.nix
@@ -2,7 +2,7 @@
 let
     updateScript = pkgs.writeShellApplication {
         name = "auto-update";
-        runtimeInputs = with pkgs; [ git gnupg git-crypt coreutils just bash nix ];
+        runtimeInputs = with pkgs; [ git gnupg git-crypt coreutils just bash nix nixos-rebuild ];
         text = builtins.readFile ./auto-update.sh;
     };
 in
