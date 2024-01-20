8ca8ba93ba31f8f7f1d6ace337353ad8d6997e5b
Bradley Nelson <bradely@nel.family>
2024-01-19 20:21:29 -0700

    fix error in parallel

--- home-manager/_mixins/programs/emulator.nix
+++ home-manager/_mixins/programs/emulator.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (retroarch.override {
-      cores = with libretro; [
-        pcsx2
-        parallel-n64
-        dolphin
-        desmume
-      ];
-    })
-    pcsx2
+    # (retroarch.override {
+    #   cores = with libretro; [
+    #     pcsx2
+    #     parallel-n64
+    #     dolphin
+    #     desmume
+    #   ];
+    # })
+    # pcsx2
     dolphin-emu
   ];
 }
