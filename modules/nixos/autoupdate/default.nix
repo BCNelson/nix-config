ce8179235856af360f62bad30eaf9cffdafe8fec
Bradley Nelson <bradely@nel.family>
2024-01-28 16:58:02 -0700

    init auto auto update nix os module

---
+++ modules/nixos/autoupdate/default.nix
@@ -0,0 +1,49 @@
+{config, lib, options, ...}:
+let
+  
+in{
+  options = { 
+    service.auto-update = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable auto-update service";
+      };
+
+    };
+   };
+
+  config =
+    let
+      updateScript = pkgs.writeShellApplication {
+        name = "auto-update";
+        runtimeInputs = with pkgs; [ git gnupg git-crypt coreutils just bash nix nixos-rebuild systemd curl hostname ];
+        text = builtins.readFile ./auto-update.sh;
+      };
+      sensitive = import ../../sensitive.nix;
+    in
+    {
+      systemd.timers.auto-update = {
+        enable = true;
+        timerConfig = {
+          OnBootSec = "1min";
+          OnUnitActiveSec = "5m";
+          Persistent = true;
+        };
+        wantedBy = [ "timers.target" ];
+      };
+
+      systemd.services.auto-update = {
+        enable = true;
+        environment = {
+          NTFY_TOPIC = sensitive.ntfy_topic;
+        };
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+          ExecStart = "${updateScript}/bin/auto-update";
+        };
+        restartIfChanged = false;
+      };
+    };
+}
