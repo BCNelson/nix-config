f45d38015735542a085d9d7d9562d9ea115b43b2
Bradley Nelson <bradely@nel.family>
2024-01-31 17:11:06 -0700

    remove errors from nix

--- nixos/common.nix
+++ nixos/common.nix
@@ -71,9 +71,4 @@
     enable = true;
     useBabelfish = true;
   };
-
-  {
-  system.activationScripts.report-changes = ''
-    ${pkgs.nix}/bin/nix profile diff-closures --profile /nix/var/nix/profiles/system
-  '';
 }
