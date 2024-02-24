7b3080501475755ceb88fe1099665b943130b832
Bradley Nelson <bradely@nel.family>
2024-02-24 11:10:57 -0700

    open firewall for ssh on servers

--- nixos/_mixins/roles/server/default.nix
+++ nixos/_mixins/roles/server/default.nix
@@ -4,7 +4,10 @@
     ../docker.nix
   ];
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+  };
   # Add nix-ld so that we can use vscode remote ssh
   programs.nix-ld.enable = true;
 }
