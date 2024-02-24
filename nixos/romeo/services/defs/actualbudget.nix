67926aed920d04429a0ad63c48580573f4ab6be0
Bradley Nelson <bradely@nel.family>
2024-02-24 10:50:16 -0700

    remove actualBudget

--- nixos/romeo/services/defs/actualbudget.nix
+++
@@ -1,8 +0,0 @@
-{ dataDirs }:
-{
-  actual-server = {
-    image = "docker.io/actualbudget/actual-server:latest-alpine";
-    volumes = [ "${dataDirs.level2}/actualBudget:/data" ];
-    restart = "unless-stopped";
-  };
-}
