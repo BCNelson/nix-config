1d598473629df5b1031e87d9fd67d417a321bb42
Bradley Nelson <bradley@nel.family>
2024-03-03 12:14:16 -0700

    update sierra to kde 6

---
+++ nixos/_mixins/roles/desktop/kde6.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+{
+  services.xserver.desktopManager.plasma6.enable = true;
+
+  services.xserver.displayManager.defaultSession = "plasma";
+
+  programs.partition-manager.enable = true;
+}
