17447dfd26b1ace8d3c35e942ee46ee0afa9248d
Bradley Nelson <bradley@nel.family>
2024-03-07 13:24:32 -0700

    romeo vlans take 2

--- nixos/romeo/default.nix
+++ nixos/romeo/default.nix
@@ -52,4 +52,40 @@ in
     restartTriggers = [ services.networkBacked ];
     restartIfChanged = false;
   };
+
+  systemd.network = {
+    netdevs = {
+      "20-vlan10" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan10";
+        };
+        vlanConfig.Id = 10;
+      };
+      "20-vlan30" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan30";
+        };
+        vlanConfig.Id = 30;
+      };
+    };
+    networks = {
+      "30-enp11s0" = {
+        matchConfig.Name = "enp11s0";
+        vlan = [
+          "vlan10"
+          "vlan30"
+        ];
+        networkConfig.LinkLocalAddressing = "no";
+        linkConfig.RequiredForOnline = "carrier";
+      };
+      "40-vlan10" = {
+        matchConfig.Name = "vlan10";
+      };
+      "40-vlan30" = {
+        matchConfig.Name = "vlan30";
+      };
+    };
+  };
 }
