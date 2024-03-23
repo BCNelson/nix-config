60659af0b5460329625e4c990904fe13f95a0d79
Bradley Nelson <bradley@nel.family>
2024-03-23 16:30:04 -0600

    fix paperless import typo

--- nixos/romeo/services/default.nix
+++ nixos/romeo/services/default.nix
@@ -21,7 +21,7 @@ let
   immich = import ./defs/immich.nix { inherit dataDirs libx; };
   dashy = import ./defs/dashy { inherit pkgs; };
   authelia = import ./defs/authelia { inherit dataDirs libx pkgs; };
-  paperless = import ./defs/paperless { inherit dataDirs libx; };
+  paperless = import ./defs/paperless.nix { inherit dataDirs libx; };
 in
 {
   networkBacked = libx.createDockerComposeStackPackage {
