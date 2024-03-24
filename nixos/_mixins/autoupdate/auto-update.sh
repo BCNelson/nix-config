1084576e8c64bec3baa31ec6b1e3954c8bdabe8a
Bradley Nelson <bradley@nel.family>
2024-03-23 23:38:50 -0600

    autoupdate restart changed to 1 min

--- nixos/_mixins/autoupdate/auto-update.sh
+++ nixos/_mixins/autoupdate/auto-update.sh
@@ -34,12 +34,12 @@ if [ "$current_system_initrd" != "$booted_system_initrd" ] ||
    [ "$current_system_kernel_modules" != "$booted_system_kernel_modules" ];
 then
     echo "Reboot required"
-    shutdown -r +2 "Rebooting for updates in 2 minutes"
+    shutdown -r +1 "Rebooting for updates in 1 minute"
     # check if NTFY_TOPIC is set
     if [ -n "$NTFY_TOPIC" ]; then
         echo "Sending notification to https://ntfy.sh/$NTFY_TOPIC"
         curl -H "X-Title: $(hostname) rebooting in 2 mins" \
-          -d "$(hostname) is rebooting in 2 mins as necessary for updates" \
+          -d "$(hostname) is rebooting in 1 min as necessary for updates" \
           "https://ntfy.sh/$NTFY_TOPIC"
     fi
 fi
