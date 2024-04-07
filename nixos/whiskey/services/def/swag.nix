d3813a98b95736e375705fa106c1dde0857502e3
Bradley Nelson <bradleynelson102@gmail.com>
2024-04-07 13:48:35 -0700

    wip swag and vaultwarden

---
+++ nixos/whiskey/services/def/swag.nix
@@ -0,0 +1,38 @@
+{ dataDirs, porkbunToken }:
+let
+  config = ".";
+in
+{
+  swag = {
+    build = "./swag";
+    container_name = "swag";
+    cap_add = [ "NET_ADMIN" ];
+    environment = [
+      "PUID=1002"
+      "PGID=1002"
+      "TZ=America/Denver"
+      "URL=nel.family"
+      "SUBDOMAINS=wildcard"
+      "VALIDATION=dns"
+      "DNSPLUGIN=porkbun"
+      "EMAIL=bradley@nel.family"
+      "DHLEVEL=2048"
+      "ONLY_SUBDOMAINS=true"
+      "STAGING=true"
+      "EXTRA_DOMAINS=health.nel.family vault.nel.family"
+      "DOCKER_MODS=linuxserver/mods:swag-auto-reload"
+    ];
+    volumes = [
+      "${dataDirs.level7}/swag:/config"
+      "${porkbunToken}/porkbun.ini:/config/dns-conf/porkbun.ini:ro"
+      "${config}/swag/nginx/proxy-confs:/config/nginx/proxy-confs:ro"
+      "${config}/swag/nginx/tailscale.conf:/config/nginx/tailscale.conf:ro"
+      "${config}/swag/nginx/internal.conf:/config/nginx/internal.conf:ro"
+    ];
+    ports = [
+      "443:443"
+      "80:80"
+    ];
+    restart = "unless-stopped";
+  };
+}
---
+++ nixos/whiskey/services/default.nix
@@ -0,0 +1,28 @@
+{ libx, dataDirs, pkgs, ... }:
+let
+  dns_linode_key = libx.getSecret ../../sensitive.nix "dns_linode_key";
+  porkbun_api_key = libx.getSecret ../sensitive.nix "porkbun_api_key";
+  porkbun_api_secret = ibx.getSecret ../sensitive.nix "porkbun_api_secret";
+  porkbunToken = pkgs.writeTextFile {
+    name = "porkbun-dns-config";
+    text = ''
+      dns_porkbun_key=${porkbun_api_key}
+      dns_porkbun_secret=${porkbun_api_secret}
+    '';
+    destination = "/porkbun.ini";
+  };
+  swag = import ./defs/swag.nix { inherit dataDirs porkbunToken; };
+in
+{
+  networkBacked = libx.createDockerComposeStackPackage {
+    name = "general";
+    src = ./defs/config;
+    dependencies = [ porkbunToken ];
+    dockerComposeDefinition = {
+      version = "3.8";
+      services = builtins.foldl' (a: b: a // b) { } [
+        swag
+      ];
+    };
+  };
+}
