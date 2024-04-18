5a38ea4c64039fc3ed6048efaaf1d5653f86c5c7
Bradley Nelson <bradley@nel.family>
2024-04-18 17:11:07 -0600

    Update desktop role configuration kde option rename

--- nixos/_mixins/roles/desktop/default.nix
+++ nixos/_mixins/roles/desktop/default.nix
@@ -1,35 +1,46 @@
-{ desktop, lib, ... }: {
+{ desktop, lib, ... }: 
+let
+  atLeast2405 = lib.versionAtLeast lib.trivial.release "24.05";
+in
+{
   imports = lib.optional (builtins.pathExists ./${desktop}.nix) ./${desktop}.nix;
-  # Enable the X11 windowing system.
-  services.xserver.enable = true; #TODO: See if this is needed
 
-  services.displayManager.sddm = {
-    enable = true;
-    autoNumlock = true;
-  };
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
+    };
+    xserver = {
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable the X11 windowing system.
+    xserver.enable = true; #TODO: See if this is needed
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+  } // (if atLeast2405 then {
+    displayManager.sddm = {
+      enable = true;
+      autoNumlock = true;
+    };
+  } else {
+    xserver.displayManager.sddm = {
+      enable = true;
+      autoNumlock = true;
+    };
+  });
 
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = lib.mkForce false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-    wireplumber.enable = true;
-  };
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
 
   programs.ssh = {
     startAgent = true;
--- nixos/_mixins/roles/desktop/kde6.nix
+++ nixos/_mixins/roles/desktop/kde6.nix
@@ -1,8 +1,13 @@
-_:
+{lib, pkgs, ...}:
 {
-  services.desktopManager.plasma6.enable = true;
-
-  services.displayManager.defaultSession = "plasma";
-
   programs.partition-manager.enable = true;
-}
+} // (
+  if lib.strings.versionAtLeast lib.trivial.release "24.05" then {
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
+    services.desktopManager.plasma6.enable = true;
+    services.displayManager.defaultSession = "plasma";
+  } else {
+    services.desktopManager.plasma6.enable = true;
+    services.xserver.displayManager.defaultSession = "plasma";
+  }
+)
