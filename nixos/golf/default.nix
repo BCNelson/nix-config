9efc828ca962408c46a200a3d883a63af2d37a04
Bradley Nelson <bradley@nel.family>
2024-04-23 00:12:52 -0600

    fix golf import

--- nixos/golf/default.nix
+++ nixos/golf/default.nix
@@ -1,12 +1,11 @@
 { ... }:
 {
   imports = [
-    ./hardware-configuration.nix
     ../_mixins/roles/docker.nix
     ../_mixins/roles/gaming.nix
     ../_mixins/roles/tailscale.nix
     ../_mixins/roles/desktop
-  ];
+  ] ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) ./hardware-configuration.nix;
   networking.firewall = {
     enable = true;
     allowedTCPPortRanges = [
