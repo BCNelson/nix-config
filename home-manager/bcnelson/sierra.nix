e603e971df27aae73e54eaede691540123716f9c
Bradley Nelson <bradley@nel.family>
2024-05-27 13:03:05 -0600

    add libreoffice

--- home-manager/bcnelson/sierra.nix
+++ home-manager/bcnelson/sierra.nix
@@ -5,6 +5,7 @@
     ../_mixins/programs/deckmaster
     ../_mixins/programs/jetbrains/dataGrip.nix
     ../_mixins/programs/jetbrains/goland.nix
+    ../_mixins/programs/libreOffice.nix
     ../_mixins/programs/emulator.nix
     ../_mixins/work/guidecx/k2.nix
   ];
