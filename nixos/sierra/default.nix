b8c2d1bfcf129eb7b830a812d067544dead78750
Bradley Nelson <bradley@nel.family>
2024-06-27 21:21:44 -0600

    add nfs settings

--- nixos/sierra/default.nix
+++ nixos/sierra/default.nix
@@ -20,6 +20,7 @@
       ../_mixins/hardware/streamdeck.nix
       ../_mixins/hardware/qmk.nix
       ../_mixins/hardware/platfromio.nix
+      ../_mixins/roles/nfs.nix
     ];
   networking.firewall = {
     enable = true;
