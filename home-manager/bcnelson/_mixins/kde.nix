17e21197532ac0d5cdb592dfcb85d91329c07757
Bradley Nelson <bradley@nel.family>
2024-07-06 10:58:39 -0600

    remove default meta+t kde short cut

--- home-manager/bcnelson/_mixins/kde.nix
+++ home-manager/bcnelson/_mixins/kde.nix
@@ -9,6 +9,7 @@
       "kwin"."Window One Desktop to the Left" = "Meta+Ctrl+Shift+Left";
       "kwin"."Window One Desktop to the Right" = "Meta+Ctrl+Shift+Right";
       "plasmashell"."show-on-mouse-pos" = "Meta+V";
+      "kwin"."Edit Tiles" = [ ];
       "services/org.kde.konsole.desktop"."_launch" = "Meta+T";
       "services/org.kde.krunner.desktop"."_launch" = "Meta+Space";
       "yakuake"."toggle-window-state" = "F12";
