bfb543dcd7527fcfa60a341bbbb31c7ac5284584
Bradley Nelson <bradley@nel.family>
2024-07-27 15:14:20 -0600

    fix redo wifi

--- nixos/redo/hardware-configuration.nix
+++ nixos/redo/hardware-configuration.nix
@@ -43,6 +43,11 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
+  hardware.wirelessRegulatoryDatabase = true;
+  boot.extraModprobeConfig = ''
+    options cfg80211 ieee80211_regdom="US"
+  '';
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
