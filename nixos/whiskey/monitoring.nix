b521d45d3d7ca5e33eb1ecfc5f6d9c98c794d02e
Bradley Nelson <bradley@nel.family>
2024-08-22 23:02:55 -0600

    feat: Add exporters and scrapeConfigs for Prometheus monitoring in whiskey

--- nixos/whiskey/monitoring.nix
+++ nixos/whiskey/monitoring.nix
@@ -26,5 +26,22 @@
   services.prometheus = {
     enable = true;
     port = 9001;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+    scrapeConfigs = [
+      {
+        job_name = "whiskey";
+        static_configs = [
+          {
+            targets = [ "127.0.0.1:9002" ];
+          }
+        ];
+      }
+    ];
   };
 }
