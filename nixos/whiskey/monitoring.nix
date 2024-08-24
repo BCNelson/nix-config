24955a84e126329de0a1fc52d6c6720e5c07a778
Bradley Nelson <bradley@nel.family>
2024-08-23 21:30:50 -0600

    loki config

--- nixos/whiskey/monitoring.nix
+++ nixos/whiskey/monitoring.nix
@@ -75,71 +75,34 @@
       server.http_listen_port = 3100;
       auth_enabled = false;
 
-      ingester = {
-        lifecycler = {
-          address = "127.0.0.1";
-          ring = {
-            kvstore = {
-              store = "inmemory";
-            };
-            replication_factor = 1;
-          };
+      common = {
+        path_prefix = "/tmp/loki";
+        storage.filesystem = {
+          chunks_directory = "/tmp/loki/chunks";
+          rules_directory = "/tmp/loki/rules";
         };
-        chunk_idle_period = "1h";
-        max_chunk_age = "1h";
-        chunk_target_size = 999999;
-        chunk_retain_period = "30s";
-        max_transfer_retries = 0;
-      };
-
-      schema_config = {
-        configs = [{
-          from = "2022-06-06";
-          store = "boltdb-shipper";
-          object_store = "filesystem";
-          schema = "v11";
-          index = {
-            prefix = "index_";
-            period = "24h";
-          };
-        }];
-      };
-
-      storage_config = {
-        boltdb_shipper = {
-          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-          cache_location = "/var/lib/loki/boltdb-shipper-cache";
-          cache_ttl = "24h";
-          shared_store = "filesystem";
-        };
-
-        filesystem = {
-          directory = "/var/lib/loki/chunks";
-        };
-      };
-
-      limits_config = {
-        reject_old_samples = true;
-        reject_old_samples_max_age = "168h";
-      };
-
-      chunk_store_config = {
-        max_look_back_period = "0s";
-      };
-
-      table_manager = {
-        retention_deletes_enabled = false;
-        retention_period = "0s";
-      };
-
-      compactor = {
-        working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
-        compactor_ring = {
+        replication_factor = 1;
+        ring = {
           kvstore = {
             store = "inmemory";
           };
+          instance_addr = "127.0.0.1";
         };
+      };
+
+      schema_config = {
+        configs = [
+          {
+            from = "2020-09-07";
+            store = "boltdb-shipper";
+            object_store = "filesystem";
+            schema = "v12";
+            index = {
+              prefix = "loki_index_";
+              period = "24h";
+            };
+          }
+        ];
       };
     };
   };
