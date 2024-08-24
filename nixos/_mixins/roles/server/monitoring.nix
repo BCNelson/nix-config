2d6acc30bf1f962fe110e94281aead89d744eccf
Bradley Nelson <bradley@nel.family>
2024-08-23 18:03:33 -0600

    feat: Add Promtail and Loki monitoring services

--- nixos/_mixins/roles/server/monitoring.nix
+++ nixos/_mixins/roles/server/monitoring.nix
@@ -10,4 +10,39 @@
       };
     };
   };
+
+  
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 3031;
+        grpc_listen_port = 0;
+      };
+      positions = {
+        filename = "/tmp/positions.yaml";
+      };
+      clients = [{
+        url = "http://whiskey.b.nel.family:3100/loki/api/v1/push";
+      }];
+      scrape_configs = [{
+        job_name = "journal";
+        journal = {
+          max_age = "12h";
+          labels = {
+            job = "systemd-journal";
+            host = "pihole";
+          };
+        };
+        relabel_configs = [{
+          source_labels = [ "__journal__systemd_unit" ];
+          target_label = "unit";
+        }];
+      }];
+    };
+  };
+
+  networking.firewall.interfaces.tailscale0 = {
+    allowedTCPPorts = [ 3031 ];
+  };
 }
