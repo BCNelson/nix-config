371dba5a8beee6ebe70b36228d31ba9af90a00ed
Bradley Nelson <bradley@nel.family>
2024-08-28 17:52:04 -0600

    feat: Enable all firmware in redo hardware configuration

--- nixos/redo/hardware-configuration.nix
+++ nixos/redo/hardware-configuration.nix
@@ -50,4 +50,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableAllFirmware = true;
 }
