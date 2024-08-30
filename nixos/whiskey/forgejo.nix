d20de540cf1e661ddc37b3832959fdd1bb817e3f
Bradley Nelson <bradley@nel.family>
2024-08-30 17:46:06 -0600

    chore: Disable user registration in forgejo.nix

--- nixos/whiskey/forgejo.nix
+++ nixos/whiskey/forgejo.nix
@@ -36,7 +36,7 @@ in
         HTTP_PORT = 3000;
       };
       # You can temporarily allow registration to create an admin user.
-      service.DISABLE_REGISTRATION = false;
+      service.DISABLE_REGISTRATION = true;
       # Add support for actions, based on act: https://github.com/nektos/act
       actions = {
         ENABLED = true;
