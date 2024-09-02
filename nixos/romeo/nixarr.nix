76a8e60426500f41a8aa8dc3963552f2d73aee74
Bradley Nelson <bradley@nel.family>
2024-09-01 23:18:56 -0600

    chore: Update wgConfigText filename in nixarr.nix file

--- nixos/romeo/nixarr.nix
+++ nixos/romeo/nixarr.nix
@@ -3,7 +3,7 @@ let
   dataDirs = import ./dataDirs.nix;
 
   wgConfig = libx.getSecret ./sensitive.nix "airdnsWGConfig";
-  wgConfigText = pkgs.writeText "airDns.conf" wgConfig;
+  wgConfigText = pkgs.writeText "wg.conf" wgConfig;
   peerPort = libx.getSecretWithDefault ./sensitive.nix "airdnsPeerPort" 0;
 in
 {
