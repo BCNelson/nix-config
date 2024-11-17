f879bef2d96e6494ccc34c49e800518005912885
Bradley Nelson <bradley@nel.family>
2024-11-17 16:12:56 -0700

    Add name attribute to actual server configuration

--- nixos/romeo/services/defs/actual.nix
+++ nixos/romeo/services/defs/actual.nix
@@ -2,6 +2,7 @@
 {
   actual_server = {
     image = "docker.io/actualbudget/actual-server:latest-alpine";
+    name = "actual-server";
     ports = ["127.0.0.1:5006:5006"];
     volumes = [
       "${dataDirs.level3}/actual:/data"
