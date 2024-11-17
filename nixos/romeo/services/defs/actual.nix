e11e80d9f00f2655408b4b462219686ad1ffb882
Bradley Nelson <bradley@nel.family>
2024-11-17 16:26:42 -0700

    Update actual server image to use GitHub Container Registry

--- nixos/romeo/services/defs/actual.nix
+++ nixos/romeo/services/defs/actual.nix
@@ -1,7 +1,7 @@
 { dataDirs }:
 {
   actual_server = {
-    image = "docker.io/actualbudget/actual-server:latest-alpine";
+    image = "ghcr.io/actualbudget/actual-server:latest";
     container_name = "actual-server";
     ports = ["127.0.0.1:5006:5006"];
     volumes = [
