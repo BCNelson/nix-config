85ce76b264c82dae66d7582a84733588c718c206
Bradley Nelson <bradley@nel.family>
2024-11-17 16:09:01 -0700

    Update actual server corrent image

--- nixos/romeo/services/defs/actual.nix
+++ nixos/romeo/services/defs/actual.nix
@@ -1,7 +1,7 @@
 { dataDirs }:
 {
   actual_server = {
-    image = "advplyr/audiobookshelf";
+    image = "docker.io/actualbudget/actual-server:latest-alpine";
     ports = ["127.0.0.1:5006:5006"];
     volumes = [
       "${dataDirs.level3}/actual:/data"
