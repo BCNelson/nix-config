2e8da316d2b70e36311540b244990cf17eb71195
Bradley Nelson <bradley@nel.family>
2024-11-27 16:21:05 -0700

    Enable DEBUG mode in healthchecks configuration

--- nixos/whiskey/healthchecks.nix
+++ nixos/whiskey/healthchecks.nix
@@ -30,6 +30,7 @@ in
         ALLOWED_HOSTS = [ "https://health.b.nel.family" ];
         SITE_ROOT = "https://health.b.nel.family";
         REGISTRATION_OPEN = false;
+        DEBUG = true;
     };
     settingsFile = config.age.secrets.healthchecks.path;
     listenAddress = "localhost";
