4d6586fae15213f3f84fefa914742cce23a96d1d
Bradley Nelson <bradley@nel.family>
2024-12-05 16:27:32 -0700

    install-system clap init

--- pkgs/install-system/src/main.rs
+++ pkgs/install-system/src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    host: String,
+    user: String,
+    target_disk: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Host: {}", args.host);
+    println!("User: {}", args.user);
+    println!("Target disk: {}", args.target_disk.display());
 }
