8ac35829154f87b88d6c8b1f2eef09edd9322eab
Bradley Nelson <bradley@nel.family>
2024-12-18 21:57:37 -0700

    fix: update data directory paths in vor configuration

--- nixos/vor/dataDirs.nix
+++ nixos/vor/dataDirs.nix
@@ -1,11 +1,11 @@
 {
   data.dirs = {
-    level1 = "/liveData/NelsonData/level1"; # Critical
-    level2 = "/liveData/NelsonData/level2"; # Important
-    level3 = "/liveData/NelsonData/level3"; # High
-    level4 = "/liveData/NelsonData/level4"; # Medium
-    level5 = "/liveData/NelsonData/level5"; # Low
-    level6 = "/liveData/NelsonData/level6"; # Replaceable
+    level1 = "/liveData/level1"; # Critical
+    level2 = "/liveData/level2"; # Important
+    level3 = "/liveData/level3"; # High
+    level4 = "/liveData/level4"; # Medium
+    level5 = "/liveData/level5"; # Low
+    level6 = "/liveData/level6"; # Replaceable
     level7 = "/cache"; # Ephemeral
   };
 }
