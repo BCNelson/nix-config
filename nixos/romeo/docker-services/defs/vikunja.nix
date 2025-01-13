c8215fce5c59267f50405709b5a7f784f876a90c
Bradley Nelson <bradley@nel.family>
2025-01-13 14:58:05 -0700

    fix: update allowed origins for CORS in Vikunja service

--- nixos/romeo/docker-services/defs/vikunja.nix
+++ nixos/romeo/docker-services/defs/vikunja.nix
@@ -10,7 +10,7 @@ let
     };
     cors = {
       enabled = true;
-      allowed_origins = [ "*" ];
+      allowed_origins = [ "*" "http://127.0.0.1:45735" ];
     };
     mailer = {
       enabled = true;
