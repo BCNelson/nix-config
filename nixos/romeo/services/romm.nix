3a10a2b44b7553b8ce0e0927855045709aa4d9d3
Bradley Nelson <bradley@nel.family>
2025-02-22 21:29:29 -0700

    Add support for WebSocket connections in romm service configuration

--- nixos/romeo/services/romm.nix
+++ nixos/romeo/services/romm.nix
@@ -121,6 +121,9 @@ in
               add_header X-XSS-Protection "1; mode=block" always;
               add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
               add_header Referrer-Policy "no-referrer-when-downgrade" always;
+
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
             '';
           };
         };
