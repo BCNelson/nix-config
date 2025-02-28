12b6fc139f84a941a993b82ba089cc62aece92ee
Bradley Nelson <bradley@nel.family>
2025-02-27 21:56:25 -0700

    Fix xdg.portal.extraPortals to use kdePackages for KDE 24.05+

--- nixos/_mixins/roles/desktop/kde6.nix
+++ nixos/_mixins/roles/desktop/kde6.nix
@@ -3,7 +3,7 @@
   programs.partition-manager.enable = true;
 } // (
   if lib.strings.versionAtLeast lib.trivial.release "24.05" then {
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
+    xdg.portal.extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
     services.desktopManager.plasma6.enable = true;
     services.displayManager.defaultSession = "plasma";
     environment.systemPackages = [
