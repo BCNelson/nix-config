7019500c17ee1fd2fc8ceb2c8eb5284bceb7f50e
Bradley Nelson <bradley@nel.family>
2025-09-01 16:04:53 -0600

    Add firefox mixin for brnelson: configure profiles, extensions, and policies

---
+++ home-manager/brnelson/_mixins/firefox.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+{
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-wayland;
+    profiles = {
+      personal = {
+        name = "Personal";
+        isDefault = true;
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "Waterford";
+              url = "https://my.waterford.org/student-home";
+            }
+          ];
+        };
+        extensions.force = true;
+        settings = {
+          # Disable the builtin Password manager
+          "signon.rememberSignons" = false;
+          "signon.rememberSignons.visibilityToggle" = false;
+          "trailhead.firstrun.didSeeAboutWelcome" = true;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.topSitesRows" = 3;
+          "extensions.formautofill.creditCards.enabled" = false;
+          "widget.use-xdg-desktop-portal.file-picker" = 1;
+        };
+      };
+      default = {
+        isDefault = false;
+        id = 999;
+        extensions.force = true;
+      };
+    };
+    policies = {
+      DisablePocket = true;
+      DisableSetDesktopBackground = true;
+      WebsiteFilter = {
+        Block = [ "<all_urls>" ];
+        Allow = [ 
+          "https://*.waterford.org/*"
+          "https://*.waterfordlabs.org/*"
+          "https://*.sentry.io/*"
+        ];
+      };
+    };
+  };
+}
--- home-manager/brnelson/desktop.nix
+++ home-manager/brnelson/desktop.nix
@@ -5,6 +5,7 @@
   imports = [
     outputs.homeModules.autostart
     ./_mixins/kde.nix
+    ./_mixins/firefox.nix
   ];
 
   home.packages = [
