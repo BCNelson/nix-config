dca924ada5301756e1d732de497b28fbad440b62
Bradley Nelson <bradley@nel.family>
2025-09-15 21:27:37 -0600

    Disable fingerprint authentication for console login on bcnelson's system to prevent login delay

--- nixos/_mixins/hardware/fingerprint.nix
+++ nixos/_mixins/hardware/fingerprint.nix
@@ -10,4 +10,6 @@
   
   # Disable fingerprint authentication for SDDM to prevent login delay
   security.pam.services.sddm.fprintAuth = false;
+  # Disable fingerprint authentication for console login to prevent login delay
+  security.pam.services.login.fprintAuth = false;
 }
