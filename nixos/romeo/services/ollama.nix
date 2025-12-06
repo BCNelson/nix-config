75c683a4c53ba4a2189a1d681013496d4c7d7813
Bradley Nelson <bradley@nel.family>
2025-12-05 23:23:42 -0700

    remove GGML_VK_DISABLE_INTEGER_DOT_PRODUCT

--- nixos/romeo/services/ollama.nix
+++ nixos/romeo/services/ollama.nix
@@ -4,19 +4,17 @@
     enable = true;
     host = "0.0.0.0";
     port = 11434;
-    # Models to automatically download on first startup
     loadModels = [ "qwen3:4b" ];
     acceleration = "vulkan";
     environmentVariables = {
-      # Enable Vulkan backend for Intel Arc GPU
+      # Enable Vulkan backend
       OLLAMA_VULKAN = "1";
       # Target B580 (xe driver, device 1) - A380 (i915) loads first as device 0
       GGML_VK_VISIBLE_DEVICES = "1";
       # Maximize GPU layer usage
       OLLAMA_GPU_LAYERS = "-1";
-
-      # https://github.com/ollama/ollama/issues/13086
-      GGML_VK_DISABLE_INTEGER_DOT_PRODUCT = "1";
+      # Note: GGML_VK_DISABLE_INTEGER_DOT_PRODUCT removed - that's for iGPUs with driver bugs,
+      # the B580 discrete GPU should support integer dot products correctly
     };
   };
 
