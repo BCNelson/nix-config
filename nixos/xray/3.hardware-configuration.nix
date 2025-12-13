53fc4d986c5ae72a8f291441f1e2edd29c58ba51
Bradley Nelson <bradley@nel.family>
2025-12-12 23:28:20 -0700

    Add autoUpdate service configuration for xray

--- nixos/xray/3.hardware-configuration.nix
+++ nixos/xray/3.hardware-configuration.nix
@@ -28,4 +28,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  services.bcnelson.autoUpdate = {
+    path = lib.mkForce "/config";
+  };
 }
