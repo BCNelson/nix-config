ea1e040164423336373d2f7ad98259157d2c2888
Bradley Nelson <bradley@nel.family>
2026-04-23 10:06:20 -0600

    gamescope

--- nixos/_mixins/roles/gaming.nix
+++ nixos/_mixins/roles/gaming.nix
@@ -2,24 +2,25 @@
 {
   nixpkgs.config.packageOverrides = pkgs: {
     steam = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        libxcursor
-        libxi
-        libxinerama
-        libxscrnsaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
-      ];
+      extraPkgs =
+        pkgs: with pkgs; [
+          libxcursor
+          libxi
+          libxinerama
+          libxscrnsaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
     };
   };
   programs.steam.enable = true;
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
   environment.systemPackages = [
-    pkgs.gamescope
     pkgs.steamtinkerlaunch
     pkgs.opentrack
   ];
